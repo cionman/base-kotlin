b7ef09ecd42d7772be2cc728ee5dbc7fd73846a5
cionman <topspin1278@naver.com>
2020-08-13 08:55:09 +0900

    ADD - Java Friendly Kotlin 예제 추가

---
+++ src/test/kotlin/com/base/kotlin/kotlinbasic/E_23_java_friendly_kotlin/package-info.java
@@ -0,0 +1,7 @@
+package com.base.kotlin.kotlinbasic.E_23_java_friendly_kotlin;
+
+
+/**
+ * 코틀린의 여러 클래스들을 Java에서 적용할때 스펙상 차이로 나타나는 문제들을 해결해나가는 어노테이션 사용예제
+ *  https://developer.android.com/codelabs/java-friendly-kotlin?return=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-week5-languages%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjava-friendly-kotlin#0
+ **/
