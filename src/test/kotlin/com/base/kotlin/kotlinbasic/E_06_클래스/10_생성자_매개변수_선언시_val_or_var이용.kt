4282bdd0924087f6a19cbf05c57a053eec7c3e46
cionman <topspin1278@naver.com>
2020-08-25 07:26:10 +0900

    ADD - 기본 생성자 val또는 var를 사용한 변수와 그렇지 않은 변수의 사용상 차이

--- src/test/kotlin/com/base/kotlin/kotlinbasic/E_06_클래스/10_생성자_매개변수_선언시_val_or_var이용.kt
+++ src/test/kotlin/com/base/kotlin/kotlinbasic/E_06_클래스/10_생성자_매개변수_선언시_val_or_var이용.kt
@@ -1,5 +1,9 @@
 package com.base.kotlin.kotlinbasic.E_06_클래스
 
+/**
+ * 기본 생성자에 val나 var를 붙이지 않는 생성자 변수는 클래스 필드에서는 사용이 가능하나 메소드에서는 사용할수 없다.
+ * 기본 생성자 변수에 val나 var키워드를 붙이면 메소드 내에서 사용이 가능하다.
+ */
 class User6(val name:String, val age:Int){
     val myName = name
 
